398aaa81f453d527a574bb494f792b7ffcb2b831
minkyu-dev <park9831.dev@gmail.com>
2023-08-31 16:48:38 +0900

    view 추가 & route 설정

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
+    <router-link to="/jobs">Jobs</router-link> |
+    <router-link to="/user">User</router-link> |
+    <router-link to="/item">Item</router-link>
+  </div>
+</template>
+
+<style scoped>
+    .header {
+        color: white;
+        background-color: #42b883;
+        display: float;
+        padding: 8px;
+    }
+    .header a {
+        color: white;
+    }
+    .header .router-link-exact-active {
+        color: #35495e;
+    }
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ import VueRouter from "vue-router";
 import NewsView from '../views/NewsView.vue';
 import AskView from '../views/AskView.vue';
 import JobsView from '../views/JobsView.vue';
+import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
 
 Vue.use(VueRouter);
 
@@ -10,6 +12,10 @@ export const router = new VueRouter({
     // mode 설정을 하지 않으면 기본적으로 hash 모드로 동작하여 URL 뒤에 #이 붙는다
     mode: 'history',
     routes: [
+        {
+            path: '/',
+            redirect: '/news'
+        },
         {
             path: '/news',
             component: NewsView
@@ -21,6 +27,14 @@ export const router = new VueRouter({
         {
             path: '/jobs',
             component: JobsView
+        },
+        {
+            path: '/user',
+            component: UserView
+        },
+        {
+            path: '/item',
+            component: ItemView
         }
     ]
 });

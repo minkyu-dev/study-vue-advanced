84627aa458e9d4f3406168aa25fa917a2ef0b339
minkyu-dev <park9831.dev@gmail.com>
2023-09-11 16:11:58 +0900

    store 속성 모듈화

---
+++ src/store/actions.js
@@ -0,0 +1,47 @@
+import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index';
+
+export default {
+    // mutations에 접근할 수 있게 context 인자를 제공함
+    FETCH_NEWS(context) {
+        fetchNewsList()
+        .then(response => {
+            console.log(response.data);
+            context.commit('SET_NEWS', response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    },
+
+    // 구조 분해 문법(Destructuring)
+    // const { commit, dispatch, getters, rootGetters, rootsState, state } = context;
+    FETCH_JOBS({ commit }) {
+        // fetchJobsList()
+        // .then(response => {
+        //     console.log(response);
+        //     context.commit('SET_JOBS', response.data);
+        // })
+            
+        // 구조 분해 문법(Destructuring)
+        // const { config, data, headers, request, status, statusText } = response;
+        fetchJobsList()
+        .then(({ data }) => {
+            console.log(data);
+            commit('SET_JOBS', data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    },
+
+    FETCH_ASK({ commit }) {
+        fetchAskList()
+        .then(({ data }) => {
+            console.log(data);
+            commit('SET_ASK', data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    },
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index';
+import * as actions from './actions.js';
+import * as mutations from './mutations.js';
 
 Vue.use(Vuex);
 
@@ -11,59 +12,11 @@ export const store = new Vuex.Store({
         jobs: [],
         ask:[],
     },
-    mutations: {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        },
-
-        SET_ASK(state, ask) {
-            state.ask = ask;
-        },
+    getters: {
+        fetchedAsk(state) {
+            return state.ask;
+        }
     },
-    actions: {
-        // mutations에 접근할 수 있게 context 인자를 제공함
-        FETCH_NEWS(context) {
-            fetchNewsList()
-            .then(response => {
-                console.log(response.data);
-                context.commit('SET_NEWS', response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-
-        FETCH_JOBS(context) {
-            fetchJobsList()
-            // .then(response => {
-            //     console.log(response);
-            //     context.commit('SET_JOBS', response.data);
-            // })
-
-            // 구조 분해 문법(Destructuring)
-            // const { config, data, headers, request, status, statusText } = response
-            .then(({ data }) => {
-                console.log(data);
-                context.commit('SET_JOBS', data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-
-        FETCH_ASK(context) {
-            fetchAskList()
-            .then(({ data }) => {
-                console.log(data);
-                context.commit('SET_ASK', data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-    }
+    mutations,
+    actions
 });
